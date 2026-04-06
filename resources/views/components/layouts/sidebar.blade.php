21fa9ea6e2e35598635203603c9ee7ac41191391
FasabihJagaddecko <deckokeren@gmail.com>
2026-04-06 22:18:02 +0700

    membuat halaman login dan komponen sidebar

---
+++ resources/views/components/layouts/sidebar.blade.php
@@ -0,0 +1,10 @@
+<div class="fixed left-0 top-0 h-screen w-64 bg-white border-r border-gray-200 flex flex-col">
+    <div class="p-6 flex items-center justify-center">
+        <img src="{{ asset('logo_smekten.png') }}" alt="" class="w-12">
+        <span class="ml-3 font-bold text-xl">Archive</span>
+    </div>
+
+    <nav class="flex-1 p-4 space-y-2 overflow-y-auto">
+
+    </nav>
+</div>
